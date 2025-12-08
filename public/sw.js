c908dfbd1ab52ffe5f5828785bcc4e2ac72990af
Kojib <kojibchat@gmail.com>
2025-12-08 03:51:20 -0500

    v10.4

--- public/sw.js
+++ public/sw.js
@@ -7,7 +7,7 @@
    - Audio/video range support; fonts cached; CDNs handled
 */
 
-const VERSION = "v10.3"; // ⬅️ bump every deploy
+const VERSION = "v10.4"; // ⬅️ bump every deploy
 const PREFIX  = "PHINETWORK";
 const PRECACHE = `${PREFIX}-precache-${VERSION}`;
 const RUNTIME  = `${PREFIX}-runtime-${VERSION}`;
