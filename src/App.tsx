2a3266ea69954a922702a2269581b2c227dc93bd
Kojib <kojibchat@gmail.com>
2025-12-17 20:23:07 -0500

    v29.1

--- src/App.tsx
+++ src/App.tsx
@@ -1345,10 +1345,10 @@ function AppChrome(): React.JSX.Element {
                       href="https://github.com/phinetwork/phi.network"
                       target="_blank"
                       rel="noreferrer"
-                      aria-label="Version 29.0.9 (opens GitHub)"
+                      aria-label="Version 29.1 (opens GitHub)"
                       title="Open GitHub"
                     >
-                      29.0.9
+                      29.1
                     </a>
                   </div>
                 </footer>
